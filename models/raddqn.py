a98d271e0bf39b949c5471c6c9622e6f6a21aebd
Dr. Biswajit Sadhu <96395651+BiswajitSadhu@users.noreply.github.com>
2023-09-15 10:48:23 +0530

    updated raddqn

--- models/raddqn.py
+++ models/raddqn.py
@@ -1,9 +1,4 @@
 import torch
-# import Gridworld_v6static_3sources
-# from Gridworld_v6static_3sources import Gridworld
-import numpy as np
-import random
-import os
 
 
 class RadDqn(torch.nn.Module):
@@ -12,25 +7,19 @@ class RadDqn(torch.nn.Module):
         self.device = device
         self.config = config
         self.mode = config["mode"]
-        # self.act_func = config["leaky"]
-        self.S1 = config["S1"]
-        self.S2 = config["S2"]
-        self.S3 = config["S3"]
         self.mem_size = config["mem_size"]
         self.num_epochs = config["num_epochs"]
         self.max_moves = config["max_moves"]
         self.batch_size = config["batch_size"]
         self.sync_freq = config["sync_freq"]
 
-        # game = Gridworld(size=10, mode=mode)
-
-        # construct the atom decoder:
         layers = []
         layerwisesize = config["layerwisesize"]
-        in_features = 500
+        in_features = config["num_pieces"]*(config['grid_dimension']**2)
         for out_size in layerwisesize:
             layers.append(torch.nn.Linear(in_features=in_features, out_features=out_size))
-            layers.append(torch.nn.LeakyReLU())
+            if out_size != layerwisesize[-1]:
+                layers.append(torch.nn.LeakyReLU())
             in_features = out_size
         self.layers = torch.nn.Sequential(*layers)
 
